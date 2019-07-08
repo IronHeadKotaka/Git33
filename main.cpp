0cdd52e496b1bc04c2ec1208f2e7312b1e20f3a6
Student <WWAARR>
2019-07-08 17:30:23 +0300

    AllInOne

--- main.cpp
+++ main.cpp
@@ -7,4 +7,25 @@ void Print(int Arr[], const int n);
 void main()
 {
 	setlocale(LC_ALL, "Rus");
+	const int n = 5;
+	int Arr[n];
+	FillRand(Arr, n);
+	Print(Arr, n);
+
+}
+void FillRand(int Arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		Arr[i] = rand() % 100;
+	}
+}
+void Print(int Arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << Arr[i] << "\t";
+	}
+	cout << endl;
+
 }
